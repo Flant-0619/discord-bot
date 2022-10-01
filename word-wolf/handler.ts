831ba522665f20b13605cd75f091982d33f51706
Flant-0619 <Flant1123@gmail.com>
2022-10-02 06:11:29 +0900

    test commit

--- word-wolf/handler.ts
+++ word-wolf/handler.ts
@@ -1,6 +1,7 @@
 import {  VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function (req:  VercelRequest, res: VercelResponse) {
-  const { name = "World" } = req.query;
-  res.send(`Hello ${name}!`);
+  console.log(req)
+  console.log(res)
+  console.log('hoge')
 }
